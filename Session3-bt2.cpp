e8a1f5ba2d5a2d10f134c48a169cca8091a00096
huylg <lamgiahuy002203@gmail.com>
2024-12-10 00:30:42 +0700

    hoàn thành bt2

---
+++ Session3-bt2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	
+	float C; 
+	printf("Moi ban nhap nhiet do \n");
+	scanf("%f",&C);
+	float F = (C * 9/5) + 32;
+	printf("Chuyen thanh do F la %f \n",F); 
+	
+} 
